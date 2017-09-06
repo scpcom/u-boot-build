c865babfe2f1d3ab0de063e4ca57e09c2be545ec
Heinrich Schuchardt <xypron.glpk@gmx.de>
2017-09-06 06:28:29 +0200

    vexpress.sh
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

---
+++ vexpress.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# tools/buildman/buildman --fetch-arch=arm-unknown-linux-gnueabi
+cd denx
+make mrproper
+tools/buildman/buildman -k -o buildman vexpress_ca15_tc2
+qemu-system-arm -M vexpress-a15 -cpu cortex-a15 \
+-kernel buildman/current/vexpress_ca15_tc2/u-boot \
+-m 1024M --nographic
+cd ..
