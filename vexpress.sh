945d357a0439cb3ca5beba4e11aee98a15ffc2ff
Heinrich Schuchardt <xypron.glpk@gmx.de>
2017-09-06 23:07:25 +0200

    0001-efi_selftest-provide-an-EFI-selftest-application.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- vexpress.sh
+++ vexpress.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 # tools/buildman/buildman --fetch-arch=arm-unknown-linux-gnueabi
 cd denx
+rm -rf buildman
 make mrproper
 tools/buildman/buildman -k -o buildman vexpress_ca15_tc2
 qemu-system-arm -M vexpress-a15 -cpu cortex-a15 \
 -kernel buildman/current/vexpress_ca15_tc2/u-boot \
--m 1024M --nographic
+-m 1024M --nographic -drive file=../img.vexpress,if=sd
 cd ..
