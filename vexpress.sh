0f1c3b6d83750c0ceb5c6aeb12d054d6ddb409a1
Heinrich Schuchardt <xypron.glpk@gmx.de>
2017-09-14 21:01:23 +0200

    vexpress.sh
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- vexpress.sh
+++ vexpress.sh
@@ -6,5 +6,5 @@ make mrproper
 tools/buildman/buildman -k -o buildman vexpress_ca15_tc2
 qemu-system-arm -M vexpress-a15 -cpu cortex-a15 \
 -kernel buildman/current/vexpress_ca15_tc2/u-boot \
--m 1024M --nographic -drive file=../img.vexpress,if=sd
+-m 1024M --nographic -sd ../img.vexpress
 cd ..
