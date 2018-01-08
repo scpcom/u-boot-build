0f88be5e07e9e62231bbca1918f92184396e5bd4
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-01-08 21:23:31 +0100

    vexpress.sh: enable network
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- vexpress.sh
+++ vexpress.sh
@@ -6,5 +6,7 @@ make mrproper
 tools/buildman/buildman -k -o buildman vexpress_ca15_tc2
 qemu-system-arm -M vexpress-a15 -cpu cortex-a15 \
 -kernel buildman/current/vexpress_ca15_tc2/u-boot \
--m 1024M --nographic -sd ../img.vexpress
+-net user -net nic,model=lan9118 \
+-m 1024M --nographic \
+-drive if=sd,file=../img.vexpress,media=disk,format=raw
 cd ..
