fcbce0ffc7fda164d3520e4dd3df58cd9f1de47a
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-01-17 07:44:11 +0100

    rm 0001-efi_loader-disable-exit-boot-services-event.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch/patch-efi-next.sh
+++ patch/patch-efi-next.sh
@@ -79,14 +79,12 @@ git am ../patch/0001-efi_loader-fix-ExitBootServices.patch
 # >>> end of block driver series
 
 git am ../patch/0001-efi_loader-correctly-call-images.patch
-git am ../patch/0001-efi_selftest-test-start-image.patch
+# git am ../patch/0001-efi_selftest-test-start-image.patch
 
 git am ../patch/0001-dm-video-Correct-color-ANSI-escape-sequence-support.patch
 git am ../patch/0001-efi_loader-memory-reservations-according-to-e820-tab.patch
 
 git am ../patch/0001-efi_loader-text-output-for-device-path-end-node.patch
-
-# git am ../patch/0001-efi_loader-disable-exit-boot-services-event.patch
 
 # Work in progress
 # git am ../patch/0001-efi_loader-debug-print-memory-map.patch
