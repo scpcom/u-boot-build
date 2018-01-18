6fa0a065439acb5165ae0d373b49792cbbae7090
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-01-18 20:54:07 +0100

    0001-efi_loader-provide-link-between-devices-and-EFI-hand.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch/patch-efi-next.sh
+++ patch/patch-efi-next.sh
@@ -71,7 +71,7 @@ git am ../patch/0001-efi_loader-make-efi_disk_create_partitions-a-global-.patch
 # git am ../patch/0001-efi_selftest-execute-only-block-device-test.patch
 git am ../patch/0001-efi_loader-correct-EFI_BLOCK_IO_PROTOCOL-definitions.patch
 git am ../patch/0001-efi_loader-provide-function-to-get-last-node-of-a-de.patch
-git am ../patch/0001-efi_loader-provide-links-between-devices-EFI-handles.patch
+git am ../patch/0001-efi_loader-provide-link-between-devices-and-EFI-hand.patch
 git am ../patch/0001-tools-provide-a-tool-to-convert-a-binary-file-to-an-.patch
 git am ../patch/0001-efi_driver-EFI-block-driver.patch
 git am ../patch/0001-efi_selftest-provide-a-test-for-block-io.patch
