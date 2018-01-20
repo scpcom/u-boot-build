c73d553155cd11691f0b534e8defd6c8f0ad50f9
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-01-20 13:46:27 +0000

    iPXE: provide VLAN commands
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- config/general.h
+++ config/general.h
@@ -2,6 +2,7 @@
 #define NSLOOKUP_CMD		/* Name resolution command */
 #define PING_CMD		/* Ping command */
 #define NTP_CMD			/* NTP commands */
+#define VLAN_CMD		/* VLAN commands */
 #define IMAGE_EFI		/* EFI image support */
 #define DOWNLOAD_PROTO_HTTPS	/* Secure Hypertext Transfer Protocol */
 #define DOWNLOAD_PROTO_FTP	/* File Transfer Protocol */
