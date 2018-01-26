a8315b72e015801ef070b8fcbb5acbd40c3077aa
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-01-26 06:37:18 +0100

    update patches list
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch/patch-efi-next.sh
+++ patch/patch-efi-next.sh
@@ -27,13 +27,14 @@ git am ../patch/0001-log-add-category-LOGC_EFI.patch
 # Rebasing needed
 # git am ../patch/0001-efi_loader-use-built-in-device-tree-in-bootefi-comma.patch
 # git am ../patch/0001-efi_selftest-check-installation-of-the-device-tree.patch
-git am ../patch/0001-efi_loader-fix-comments-indent_string.patch
-git am ../patch/0001-efi_loader-catch-mispelled-bootefi-subcommand.patch
 
+# Accepted
+# git am ../patch/0001-efi_loader-fix-comments-indent_string.patch
+# git am ../patch/0001-efi_loader-catch-mispelled-bootefi-subcommand.patch
+# Superseded
 # git am ../patch/0001-efi_loader-always-call-Exit-after-an-image-returns.patch
 git am ../patch/0001-vsprintf.c-correct-printing-of-a-NULL-device-path.patch
 
-git am ../patch/0001-efi_loader-always-call-Exit-after-an-image-returns.patch
 git am ../patch/0001-efi_loader-add-a-README.iscsi-describing-booting-via.patch
 git am ../patch/0001-efi_loader-split-README.efi-into-two-separate-docume.patch
 git am ../patch/0001-efi_loader-rewrite-README.efi.patch
