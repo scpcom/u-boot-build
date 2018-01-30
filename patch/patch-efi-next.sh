ee33733e221f063b9cea058bc0f50b25193d52f2
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-01-30 19:49:24 +0100

    0001-efi_loader-fix-the-online-help-for-bootefi-bootmgr.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch/patch-efi-next.sh
+++ patch/patch-efi-next.sh
@@ -26,6 +26,7 @@ git am ../patch/0001-dm-video-use-constants-to-refer-to-colors.patch
 git am ../patch/0001-dm-video-support-increased-intensity-bold.patch
 
 git am ../patch/0001-efi_loader-remove-deprecated-ConsoleControlProtocol.patch
+git am ../patch/0001-efi_loader-fix-the-online-help-for-bootefi-bootmgr.patch
 # git am ../patch/0001-efi_loader-clean-up-efi_exit.patch
 # git am ../patch/0001-efi_loader-correct-return-types-do_bootefi_exec.patch
 # git am ../patch/0001-efi_loader-call-efi_setup_loaded_image-first.patch
