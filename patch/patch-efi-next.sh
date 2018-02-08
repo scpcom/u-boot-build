d72305744e82a87b693fafc051506892af85e44f
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-02-08 23:03:28 +0100

    0001-efi_loader-return-efi_status_t-from-efi_gop_register.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch/patch-efi-next.sh
+++ patch/patch-efi-next.sh
@@ -1,4 +1,26 @@
 #!/bin/sh
+
+# git am ../patch/0001-Test-vexpress-only.patch
+git am ../patch/0001-vexpress_ca15_tc2_defconfig-build-EFI.patch
+
+# Fix building u-boot.rom for qemu-x86_64_defconfig
+git am ../patch/0001-spl-u-boot-spl-nodtb.bin-remove-.got-and-.got.plt-se.patch
+
+# Support EFI block device on BananaPi
+git am ../patch/0001-mmc-sunxi-support-cd-inverted.patch
+git am ../patch/0001-configs-sunxi-enable-BLK-DM_MMC-for-Bananapi.patch
+
+# Various fixes
+git am ../patch/0001-ubifs-avoid-possible-NULL-dereference.patch
+git am ../patch/0001-mmc-meson_gx_mmc-avoid-division-by-zero.patch
+git am ../patch/0001-rockchip-gpio-remove-dead-code.patch
+
+# Video console
+git am ../patch/0001-dm-video-show-correct-colors-in-graphical-console.patch
+git am ../patch/0001-dm-video-correctly-clean-background-in-16bit-mode.patch
+git am ../patch/0001-dm-video-use-constants-to-refer-to-colors.patch
+git am ../patch/0001-dm-video-support-increased-intensity-bold.patch
+
 # EFI for v2018.03
 ## Accepted by Alex
 git am ../patch/0001-efi_loader-fix-building-crt0-on-arm.patch
@@ -8,31 +30,11 @@ git am ../patch/0001-efi_selftest-use-correct-compiler-flags-for-miniapps.patch
 git am ../patch/0001-efi_loader-add-missing-runtime-services-stubs.patch
 git am ../patch/0001-efi_loader-create-stub-for-CreateEventEx.patch
 git am ../patch/0001-efi_driver-return-type-of-efi_driver_init.patch
-git am ../patch/0001-efi_loader-add-missing-EFI_RESET_PLATFORM_SPECIFIC.patch
-
-# Support EFI block device on BananaPi
-git am ../patch/0001-mmc-sunxi-support-cd-inverted.patch
-git am ../patch/0001-configs-sunxi-enable-BLK-DM_MMC-for-Bananapi.patch
-
-git am ../patch/0001-ubifs-avoid-possible-NULL-dereference.patch
-git am ../patch/0001-mmc-meson_gx_mmc-avoid-division-by-zero.patch
-
-# git am ../patch/0001-Test-vexpress-only.patch
-git am ../patch/0001-vexpress_ca15_tc2_defconfig-build-EFI.patch
-
-# Fix building u-boot.rom for qemu-x86_64_defconfig
-git am ../patch/0001-spl-u-boot-spl-nodtb.bin-remove-.got-and-.got.plt-se.patch
-
-git am ../patch/0001-rockchip-gpio-remove-dead-code.patch
-
-git am ../patch/0001-dm-video-show-correct-colors-in-graphical-console.patch
-git am ../patch/0001-dm-video-correctly-clean-background-in-16bit-mode.patch
-git am ../patch/0001-dm-video-use-constants-to-refer-to-colors.patch
-git am ../patch/0001-dm-video-support-increased-intensity-bold.patch
-
-git am ../patch/0001-efi_loader-remove-deprecated-ConsoleControlProtocol.patch
 git am ../patch/0001-efi_loader-split-README.efi-into-two-separate-docume.patch
 git am ../patch/0001-efi_loader-rewrite-README.efi.patch
+
+git am ../patch/0001-efi_loader-add-missing-EFI_RESET_PLATFORM_SPECIFIC.patch
+git am ../patch/0001-efi_loader-remove-deprecated-ConsoleControlProtocol.patch
 
 # From Simon's Sandbox support series
 git am ../patch/0001-efi_loader-simplify-calling-efi_init_obj_list.patch
@@ -41,6 +43,9 @@ git am ../patch/0001-efi_loader-use-constants-in-efi_allocate_pages.patch
 git am ../patch/0001-efi_loader-exit-status-for-efi_reset_system_init.patch
 git am ../patch/0001-efi_loader-correct-efi_disk_register.patch
 git am ../patch/0001-efi-Correct-header-order-in-efi_memory.patch
+git am ../patch/0001-efi_loader-efi_get_time_init-should-return-status-co.patch
+git am ../patch/0001-efi_loader-return-efi_status_t-from-efi_gop_register.patch
+git am ../patch/0001-efi_loader-check-initialization-of-EFI-subsystem-is-.patch
 
 # From Robert's Shell support series 
 git am ../patch/0001-efi_loader-Initial-EFI_DEVICE_PATH_UTILITIES_PROTOCO.patch
@@ -49,8 +54,6 @@ git am ../patch/0001-efi_loader-do-not-use-2.0.5-as-UEFI-revision-number.patch
 git am ../patch/0001-efi_loader-show-UEFI-revision-in-helloworld.patch
 git am ../patch/0001-efi_loader-provide-firmware-revision.patch
 
-git am ../patch/0001-efi_loader-efi_get_time_init-should-return-status-co.patch
-git am ../patch/0001-efi_loader-check-initialization-of-EFI-subsystem-is-.patch
 git am ../patch/0001-efi_loader-do_bootefi_exec-should-always-return-an-E.patch
 
 git am ../patch/0001-efi_loader-support-device-tree-for-bootefi-selftest.patch
