db3992810f5f07eacb438d02b6f9e4692a630d3c
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-02-12 08:16:58 +0100

    Sequence of patches
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch/patch-efi-next.sh
+++ patch/patch-efi-next.sh
@@ -24,20 +24,31 @@ git am ../patch/0001-dm-video-support-increased-intensity-bold.patch
 # EFI
 git am ../patch/0001-efi_loader-delete-doc-README.efi.patch
 git am ../patch/0001-efi_loader-provide-new-doc-README.efi.patch
-git am ../patch/0001-efi_loader-add-missing-EFI_RESET_PLATFORM_SPECIFIC.patch
-git am ../patch/0001-efi_loader-remove-deprecated-ConsoleControlProtocol.patch
 
-# From Simon's Sandbox support series
-git am ../patch/0001-efi_loader-simplify-calling-efi_init_obj_list.patch
+# cmd/bootefi series
 git am ../patch/0001-efi_loader-efi_smbios_register-should-have-a-return-.patch
-git am ../patch/0001-efi_loader-use-constants-in-efi_allocate_pages.patch
-git am ../patch/0001-efi_loader-exit-status-for-efi_reset_system_init.patch
-git am ../patch/0001-efi-Correct-header-order-in-efi_memory.patch
-git am ../patch/0001-efi_loader-efi_get_time_init-should-return-status-co.patch
 git am ../patch/0001-efi_loader-return-efi_status_t-from-efi_gop_register.patch
 git am ../patch/0001-efi_loader-return-efi_status_t-from-efi_net_register.patch
 git am ../patch/0001-efi_loader-consistently-return-efi_status_t-efi_watc.patch
+git am ../patch/0001-efi_loader-simplify-calling-efi_init_obj_list.patch
+git am ../patch/0001-efi_loader-use-constants-in-efi_allocate_pages.patch
+git am ../patch/0001-efi_loader-exit-status-for-efi_reset_system_init.patch
+git am ../patch/0001-efi_loader-efi_get_time_init-should-return-status-co.patch
+git am ../patch/0001-efi_loader-do_bootefi_exec-should-always-return-an-E.patch
 git am ../patch/0001-efi_loader-check-initialization-of-EFI-subsystem-is-.patch
+git am ../patch/0001-efi_loader-support-device-tree-for-bootefi-selftest.patch
+git am ../patch/0001-efi_selftest-check-installation-of-the-device-tree.patch
+
+# diverse protocols
+git am ../patch/0001-efi_loader-add-missing-EFI_RESET_PLATFORM_SPECIFIC.patch
+git am ../patch/0001-efi_loader-remove-deprecated-ConsoleControlProtocol.patch
+git am ../patch/0001-efi_loader-show-UEFI-revision-in-helloworld.patch
+git am ../patch/0001-efi_loader-provide-firmware-revision.patch
+git am ../patch/0001-efi_loader-implement-missing-bit-blit-operations-in-.patch
+git am ../patch/0001-efi_selftest-test-gop-bitblt.patch
+
+# From Simon's Sandbox support series
+git am ../patch/0001-efi-Correct-header-order-in-efi_memory.patch
 git am ../patch/0001-efi-sandbox-Adjust-memory-usage-for-sandbox.patch # no change
 git am ../patch/0001-sandbox-smbios-Update-to-support-sandbox.patch # no change
 git am ../patch/0001-sandbox-Add-a-setjmp-implementation.patch # no change
@@ -46,17 +57,6 @@ git am ../patch/0001-efi-sandbox-add-distroboot-support.patch
 
 # From Robert's Shell support series 
 git am ../patch/0001-efi_loader-Initial-EFI_DEVICE_PATH_UTILITIES_PROTOCO.patch
-
-git am ../patch/0001-efi_loader-show-UEFI-revision-in-helloworld.patch
-git am ../patch/0001-efi_loader-provide-firmware-revision.patch
-
-git am ../patch/0001-efi_loader-do_bootefi_exec-should-always-return-an-E.patch
-
-git am ../patch/0001-efi_loader-support-device-tree-for-bootefi-selftest.patch
-git am ../patch/0001-efi_selftest-check-installation-of-the-device-tree.patch
-
-git am ../patch/0001-efi_loader-implement-missing-bit-blit-operations-in-.patch
-git am ../patch/0001-efi_selftest-test-gop-bitblt.patch
 
 # To be reworked
 # git am ../patch/0001-efi_loader-memory-reservations-according-to-e820-tab.patch
