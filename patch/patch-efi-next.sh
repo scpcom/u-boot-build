97235e3775ad64e8a13da496d2087dad738059f6
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-02-12 07:53:15 +0100

    Remove accepted patches
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch/patch-efi-next.sh
+++ patch/patch-efi-next.sh
@@ -21,21 +21,9 @@ git am ../patch/0001-dm-video-correctly-clean-background-in-16bit-mode.patch
 git am ../patch/0001-dm-video-use-constants-to-refer-to-colors.patch
 git am ../patch/0001-dm-video-support-increased-intensity-bold.patch
 
-# EFI for v2018.03
-## Accepted by Alex
-git am ../patch/0001-efi_loader-fix-building-crt0-on-arm.patch
-git am ../patch/0001-efi_loader-fix-the-online-help-for-bootefi-bootmgr.patch
-## Open
-git am ../patch/0001-efi_selftest-use-correct-compiler-flags-for-miniapps.patch
-git am ../patch/0001-efi_loader-add-missing-runtime-services-stubs.patch
-git am ../patch/0001-efi_loader-create-stub-for-CreateEventEx.patch
-git am ../patch/0001-efi_driver-return-type-of-efi_driver_init.patch
-git am ../patch/0001-efi_loader-correct-efi_disk_register.patch
-git am ../patch/0001-efi_loader-split-README.efi-into-two-separate-docume.patch
+# EFI
 git am ../patch/0001-efi_loader-delete-doc-README.efi.patch
 git am ../patch/0001-efi_loader-provide-new-doc-README.efi.patch
-
-git am ../patch/0001-efi_loader-do-not-use-2.0.5-as-UEFI-revision-number.patch
 git am ../patch/0001-efi_loader-add-missing-EFI_RESET_PLATFORM_SPECIFIC.patch
 git am ../patch/0001-efi_loader-remove-deprecated-ConsoleControlProtocol.patch
 
