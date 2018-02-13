3b1d0e1e39a56dae5281374c7fc28f7ebe4713c8
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-02-13 19:27:15 +0100

    0001-sandbox-Add-a-setjmp-implementation.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch/patch-efi-next.sh
+++ patch/patch-efi-next.sh
@@ -15,6 +15,8 @@ git am ../patch/0001-ubifs-avoid-possible-NULL-dereference.patch
 git am ../patch/0001-mmc-meson_gx_mmc-avoid-division-by-zero.patch
 git am ../patch/0001-rockchip-gpio-remove-dead-code.patch
 git am ../patch/0001-dm-core-fix-typo-in-comment-device.h.patch
+git am ../patch/0001-fs-fat-avoid-useless-conversion-when-calling-getclus.patch
+git am ../patch/0001-fs-fat-avoid-superfluous-conversion-calling-set_clus.patch
 
 # Video console
 git am ../patch/0001-dm-video-show-correct-colors-in-graphical-console.patch
