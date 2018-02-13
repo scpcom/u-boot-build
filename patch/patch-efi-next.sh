4b4340aa2dd93a62121a6a52b1cd534066bd9a3d
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-02-13 20:05:32 +0100

    0001-efi-sandbox-no-support-for-DHCP-EFI-boot.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch/patch-efi-next.sh
+++ patch/patch-efi-next.sh
@@ -62,6 +62,7 @@ git am ../patch/0001-efi-sandbox-add-distroboot-support.patch
 git am ../patch/0001-Define-board_quiesce_devices-in-a-shared-location.patch
 git am ../patch/0001-Add-a-comment-for-board_quiesce_devices.patch # no change
 git am ../patch/0001-efi-sandbox-Add-relocation-constants.patch # no change
+git am ../patch/0001-efi-sandbox-no-support-for-DHCP-EFI-boot.patch
 git am ../patch/0001-efi-sandbox-Enable-EFI-loader-builder-for-sandbox.patch # rebased
 
 # From Robert's Shell support series 
