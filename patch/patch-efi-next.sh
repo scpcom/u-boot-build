4f66043eb41980c4c2deff79fd3285163befc344
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-02-17 23:07:06 +0100

    0001-efi_loader-check-parameter-in-InstallConfigurationTa.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch/patch-efi-next.sh
+++ patch/patch-efi-next.sh
@@ -25,6 +25,7 @@ git am ../patch/0001-dm-video-support-increased-intensity-bold.patch
 # EFI
 git am ../patch/0001-efi_loader-delete-doc-README.efi.patch
 git am ../patch/0001-efi_loader-provide-new-doc-README.efi.patch
+git am ../patch/0001-efi_loader-check-parameter-in-InstallConfigurationTa.patch
 
 # cmd/bootefi series
 git am ../patch/0001-efi_loader-efi_smbios_register-should-have-a-return-.patch
