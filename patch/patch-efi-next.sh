d1a51ebf8d42e3a0fff5bd7e89b42358e1450963
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-03-07 03:15:51 +0100

    0001-yaffs2-iterator-variable-cannot-be-NULL.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch/patch-efi-next.sh
+++ patch/patch-efi-next.sh
@@ -15,6 +15,8 @@ git am ../patch/0001-ubifs-avoid-possible-NULL-dereference.patch
 git am ../patch/0001-mmc-meson_gx_mmc-avoid-division-by-zero.patch
 git am ../patch/0001-fs-fat-avoid-useless-conversion-when-calling-get_clu.patch
 git am ../patch/0001-fs-fat-avoid-superfluous-conversion-calling-set_clus.patch
+git am ../patch/0001-scripts-coccinelle-add-some-more-coccinelle-tests.patch
+git am ../patch/0001-yaffs2-iterator-variable-cannot-be-NULL.patch
 
 # Video console
 git am ../patch/0001-dm-video-show-correct-colors-in-graphical-console.patch
